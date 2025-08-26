f5fe2e0572aa72e325dab76ab732d70ba3e7c81b
Himanshu Chuchra <code@himanshuchuchra.com>
2025-08-27 01:55:08 +0530

    Create Crust Model Schema

---
+++ src/models/crust.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { type Schema } from "mongoose";
+
+const crustSchema: Schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    prices: [
+      {
+        sizeId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Size",
+          required: true,
+        },
+        price: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    isAvailable: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Crust = mongoose.model("Crust", crustSchema);
+export default Crust;
