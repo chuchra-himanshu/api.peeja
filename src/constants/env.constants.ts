fb53259e3d82ac4a87dd7c7c87599e81bc7f7f4a
Himanshu Chuchra <code@himanshuchuchra.com>
2025-08-27 01:27:26 +0530

    Create Pizza Model Schema

--- src/constants/env.constants.ts
+++ src/constants/env.constants.ts
@@ -1,4 +1,7 @@
-const PORT: number = Number(process.env.PORT) || 8080;
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 3000;
 
 export default {
   PORT,
---
+++ src/models/pizza.model.ts
@@ -0,0 +1,85 @@
+import mongoose, { type Schema } from "mongoose";
+
+const pizzaSchema: Schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    image: {
+      url: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+    sizes: [
+      {
+        title: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        slug: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        radius: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    description: {
+      type: String,
+      required: true,
+    },
+    crust: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Crust",
+      required: true,
+    },
+    addOns: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Crust",
+        required: true,
+      },
+    ],
+    toppings: [
+      {
+        toppingId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Topping",
+          required: true,
+        },
+        optional: {
+          type: Boolean,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      enum: ["VEG", "NON-VEG", "VEGAN"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Pizza = mongoose.model("Pizza", pizzaSchema);
+export default Pizza;
